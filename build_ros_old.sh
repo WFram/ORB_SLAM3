7e7e9601214afdff293d09087a434f95b99cc434
WFram <earl.freedom.ea@gmail.com>
2022-02-03 21:42:21 +0300

    Adjusted for OpenCV 4.2.0

--- build_ros_old.sh
+++ build_ros_old.sh
@@ -1,7 +1,9 @@
+export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:${HOME}/ORB_SLAM3/Examples_old/ROS
+
 echo "Building ROS nodes"
 
 cd Examples_old/ROS/ORB_SLAM3
 mkdir build
 cd build
 cmake .. -DROS_BUILD_TYPE=Release -DPYTHON_EXECUTABLE:FILEPATH=/usr/bin/python3
-make -j
+make -j6
